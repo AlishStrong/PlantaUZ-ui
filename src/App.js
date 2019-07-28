bcd8d00fe93766dc9edcb4a49f2702b2630697a7
AlishStrong <alishet1990@gmail.com>
2019-07-28 22:36:55 +0300

    Installed react-simple-maps. Added map of Uzbekistan with regions and districts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UzbekistanMap from './UzbekistanMap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to PlantaUz</h1>
-        <p>Alisher added feature changes</p>
-        <p>Develop from branch Thi-feature </p>
-        <p>Second push from Thi after got deleted</p>
-        <h2>Thi-feature-code</h2>
-        <p>Second commit from thi-feature-code brand</p>
-        <h3>Thi feature NEW base</h3>
-        <h2>Alisher-feature-oldbase</h2>
       </header>
+      <UzbekistanMap />
     </div>
   );
 }
